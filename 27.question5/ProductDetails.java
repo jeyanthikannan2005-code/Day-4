28a8045cd25dbebf4c44c77724e3329373473251
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-27 15:24:10 +0530

    Product Details Program

---
+++ 27.question5/ProductDetails.java
@@ -0,0 +1,18 @@
+class ProductDetails
+{
+	public static void main(String[] args)
+	{
+		int StoreProductID=276;
+		String ProductName="Laptop";
+		double ProductPrice=70000.88;
+		float ProductRating=4.4f;
+		Boolean ProductAvailability=true;
+		System.out.println(StoreProductID);
+		System.out.println(ProductName);
+		System.out.println(ProductPrice);
+		System.out.println(ProductRating);
+		System.out.println(ProductAvailability);
+	}
+}
+
+	 
