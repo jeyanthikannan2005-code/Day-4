4aa9f357c2615b67f600033dc7fa1d31ceeabf97
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-27 15:20:17 +0530

    Declare,Assign and print all details

---
+++ 27.question1/StudentDetails.java
@@ -0,0 +1,30 @@
+class StudentDetails
+{
+	public static void main(String[] args)
+	{
+		//Declaration
+		int StudentID;
+		String Name;
+		byte Age;
+		float Percentage;
+		char Grade;
+		//Assigning values
+		StudentID=101;
+		Name="Saranya";
+		Age=20;
+		Percentage=85.6f;
+		Grade='A';
+		System.out.println("Student ID: "+StudentID);
+		System.out.println("Name: "+Name);
+		System.out.println("Age: "+Age);
+		System.out.println("Percentage: "+Percentage);
+		System.out.println("Grade: "+Grade);
+	}
+}
+
+		
+
+
+
+
+		
