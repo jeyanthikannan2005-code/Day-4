a62f96259cdf33bf91cf89bd7feedf7ca8c27e1b
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-27 15:22:15 +0530

    Bank Account Program

---
+++ 27.question3/BankAccount.java
@@ -0,0 +1,19 @@
+class BankAccount
+{
+	public static void main(String[] args)
+	{
+	long AccountNumber=765432490l;
+	String AccountHolderName="Priya";
+	double AccountBalance=68000.89;
+	String AccountType="Savings";
+	System.out.println(AccountNumber);
+	System.out.println(AccountHolderName);
+	System.out.println(AccountBalance);
+	System.out.println(AccountType);
+	}
+}
+
+
+	   
+
+	
