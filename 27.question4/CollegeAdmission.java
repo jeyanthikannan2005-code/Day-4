b7da0bac00dc5670bbb329573e59e07cd1aa6463
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-02-27 15:23:15 +0530

    College admission Program

---
+++ 27.question4/CollegeAdmission.java
@@ -0,0 +1,17 @@
+class CollegeAdmission
+{
+	public static void main(String[] args)
+	{
+		long RegisterNumber=276589;
+		String StudentName="Shreeja";
+		String CourseName="B.Tech";
+		double Fees=65000.8;
+		Boolean HostelRequired=false;
+		System.out.println(RegisterNumber);
+		System.out.println(StudentName);
+		System.out.println(CourseName);
+		System.out.println(Fees);
+		System.out.println(HostelRequired);
+
+	}
+}
